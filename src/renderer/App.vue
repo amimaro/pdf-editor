b7ac43de9911c01cead6df3e3a1e8110fe00c730
Amimaro <amir.zahlan@gmail.com>
2018-04-21 11:11:02 -0300

    :tada: :pencil:

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'pdf-editor'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
