71038c12a895238e2211c11d4b4e50352f9291a0
Amimaro <amir.zahlan@gmail.com>
2018-04-22 10:41:46 -0300

    Update landing page :art:

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,21 +1,10 @@
 <template>
-<div id="landing-page">
-  <center>
-    <div class="table">
-      <div class="row">
-        <div class="element-cell" style="padding-right: 15px">
-          <h1>
-          Drop your file in the box below or
-        </h1>
-        </div>
-        <div class="cell">
-          <input type="file">
-        </div>
-      </div>
-    </div>
-    <div id="drop-file">
-    </div>
-  </center>
+<div class="drop-file" @dragover.prevent @drop="onDrop">
+  <div class="helper"></div>
+  <label class="btn">
+	    Select or Drop a PDF
+	    <input type="file" @change="onChange">
+  </label>
 </div>
 </template>
 
@@ -23,9 +12,27 @@
 export default {
   name: 'landing-page',
   components: {},
-  methods: {},
   data() {
     return {}
+  },
+  methods: {
+    onDrop: function (event) {
+      event.stopPropagation()
+      event.preventDefault()
+      let files = event.dataTransfer.files
+      this.loadFile(files[0])
+    },
+    onChange(event) {
+      let files = event.target.files
+      this.loadFile(files[0])
+    },
+    loadFile(file) {
+      if (!file.type.match('application/pdf')) {
+        alert('Select a PDF')
+        return
+      }
+      console.log(file)
+    }
   }
 }
 </script>
@@ -40,19 +47,73 @@ export default {
 body {
   font-family: 'Quicksand', sans-serif;
   padding: 10px;
+  height: 100%;
+  text-align: center;
 }
 
-#landing-page {
-
+.btn {
+  background-color: #4c39d3;
+  border: 0;
+  color: #fff;
+  cursor: pointer;
+  display: inline-block;
+  font-weight: bold;
+  padding: 15px 35px;
+  position: relative;
 }
 
-#drop-file {
-  width: 150vh;
+.btn:hover {
+  background-color: #402072;
+}
+
+input[type="file"] {
+  position: absolute;
+  opacity: 0;
+  z-index: -1;
+}
+
+.align-center {
+  text-align: center;
+}
+
+.helper {
+  height: 100%;
+  display: inline-block;
+  vertical-align: middle;
+  width: 0;
+}
+
+.hidden {
+  display: none !important;
+}
+
+.hidden.image {
+  display: inline-block !important;
+}
+
+.display-inline {
+  display: inline-block;
+  vertical-align: middle;
+}
+
+.img {
+  border: 1px solid #f6f6f6;
+  display: inline-block;
+  height: auto;
+  max-height: 80%;
+  max-width: 80%;
+  width: auto;
+}
+
+.drop-file {
+  margin-top: 20px;
+  background-color: #f0f2ff;
+  border: 10px dashed #aaa;
+  border-radius: 10px;
   height: 80vh;
-  margin-top: 10px;
-  border-style: dashed;
-  border-color: gray;
-  color: gray;
+  width: 100vh;
+  text-align: center;
+  display: inline-block;
   vertical-align: middle;
 }
 </style>
